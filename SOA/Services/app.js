0224884d8c4b9f11a25f3906bb429bfe39e6f60d
on2e <gthomados@gmail.com>
2021-05-08 20:17:18 +0300

    Add initial Services dir structure

---
+++ SOA/Services/app.js
@@ -0,0 +1,54 @@
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const questionRouter = require('./routes/services/question');
+const answerRouter = require('./routes/services/answer');
+const browseRouter = require('./routes/services/browse');
+const accountRouter = require('./routes/services/account');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+/**
+ * -------------- ROUTES --------------
+ */
+
+app.use('/question', questionRouter);
+app.use('/answer', answerRouter);
+app.use('/browse', browseRouter);
+app.use('/account', accountRouter);
+
+/**
+ * -------------- ERROR HANDLER --------------
+ */
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// log errors
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  if (status >= 500 && req.app.get('env') === 'development') {
+    console.error(err.stack);
+  }
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status;
+  res.status(status);
+  const message = status >= 500 ? "Something's wrong" : err.message;
+  const expose = status >= 500 && req.app.get('env') === 'development';
+  res.end(expose ? message + '\n\n' + err.stack : message);
+});
+
+module.exports = app;
