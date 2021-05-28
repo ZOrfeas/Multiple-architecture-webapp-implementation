1cc9942a8d96c8eea7fd83366b2f32e43d725e4f
ZOrfeas <orfeas.zografos@gmail.com>
2021-05-29 01:58:36 +0300

    Fix indentation

--- SOA/Services/swagger.js
+++ SOA/Services/swagger.js
@@ -1,27 +1,27 @@
 const swaggerAutogen = require('swagger-autogen')({
-    openapi: "3.0.0",
-    disableLogs: false,
+  openapi: "3.0.0",
+  disableLogs: false,
 });
 
 const doc = {
-    info: {
-        title: 'Service endpoints',
-        description: 'Endpoint capabilities of the Services Component for the askMeAnything SOA implementation',
-    },
-    host: 'localhost:3000',
-    basepath: '/',
-    schemes: ['http'],
-    tags: [
-        { name: 'Account' },
-        { name: 'Answer' },
-        { name: 'Question' },
-        { name: 'Browse' },
-    ]
+  info: {
+    title: 'Service endpoints',
+    description: 'Endpoint capabilities of the Services Component for the askMeAnything SOA implementation',
+  },
+  host: 'localhost:3000',
+  basepath: '/',
+  schemes: ['http'],
+  tags: [
+    { name: 'Account' },
+    { name: 'Answer' },
+    { name: 'Question' },
+    { name: 'Browse' },
+  ],
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./app.js']
+const endpointsFiles = ['./app.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    require('./bin/www')
+  require('./bin/www');
 });
