67ffa852f8290a62c7ed41c0b970d3671e661ec1
on2e <gthomados@gmail.com>
2021-05-29 22:44:10 +0300

    Conditional render of user in navbar

--- SOA/frontend/src/Nav/Nav.js
+++ SOA/frontend/src/Nav/Nav.js
@@ -1,17 +1,35 @@
 import './Nav.css'
+import { useAuth } from '../AuthContext';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavComponent() {
+  const { user } = useAuth();
   return (
       <Navbar className="fixed-top" bg="dark" variant="dark">
         <Navbar.Brand href="/">AskMeAnything</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
         </Nav>
-        <Button className="mr-2" variant="outline-primary">Sign In</Button>
-        <Button variant="primary" href="/signup">Sign Up</Button>
+          {user ?
+              <Nav>
+                <NavDropdown title={user.displayName} id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#">
+                    <i className="material-icons-sharp mr-3">account_circle</i>Account
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#">
+                    <i className="material-icons-sharp mr-3">logout</i>Log out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              :
+              <Nav>
+                <Button className="mr-2" variant="outline-primary" href="#">Sign In</Button>
+                <Button variant="primary" href="/signup">Sign Up</Button>
+              </Nav>
+          }
       </Navbar>
   );
 }
