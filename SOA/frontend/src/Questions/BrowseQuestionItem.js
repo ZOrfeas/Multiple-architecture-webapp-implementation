7c254b709d59eb17652245995d0c293bb6f715ff
on2e <gthomados@gmail.com>
2021-06-11 22:24:48 +0300

    Get questions with specific keyword feature

--- SOA/frontend/src/Questions/BrowseQuestionItem.js
+++ SOA/frontend/src/Questions/BrowseQuestionItem.js
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom'
 import Badge from 'react-bootstrap/Badge'
+import React from "react";
 
-function BrowseQuestionItem({ id, title, summary, keywords, askedOn, askedBy, answerCount }) {
-  const dateFormat = date => {
+function BrowseQuestionItem({ id, title, summary, keywords, setId, setPage, askedOn, askedBy, answerCount }) {
+  const formatDate = date => {
     const dateObj = new Date(date.split('.')[0])
     const [, month, dayNum, year] = dateObj.toDateString().split(' ')
     return `${month}, ${dayNum} ${year}`
   }
+
+  const formatName = name => name.split(' ').join('-')
 
   return (
       <div className='d-flex'>
@@ -15,10 +18,23 @@ function BrowseQuestionItem({ id, title, summary, keywords, askedOn, askedBy, an
           <p className='question-summary small mb-2'>{summary}</p>
           <div className='keywords mb-2'>
             {keywords.map(keyword => (
-                <Badge key={keyword.id} className='keyword-badge mr-1'>{keyword.name}</Badge>
+                <Badge key={keyword.id} className='keyword-badge mr-1'>
+                  <Link
+                      to={{
+                        pathname: `/questions/keyword/${formatName(keyword.name)}`,
+                        state: { id: keyword.id, name: keyword.name }
+                      }}
+                      onClick={e => {
+                        setId(keyword.id)
+                        setPage(1)
+                      }}
+                  >
+                    {keyword.name}
+                  </Link>
+                </Badge>
             ))}
           </div>
-          <p className='date-user text-muted mb-0 px-0'>asked on {dateFormat(askedOn)} by {askedBy ? askedBy : '[deleted]'}</p>
+          <p className='date-user text-muted mb-0 px-0'>asked on {formatDate(askedOn)} by {askedBy ? askedBy : '[deleted]'}</p>
         </div>
         <div className='stats d-none d-md-block flex-shrink-0 text-right ml-3 p-2'>
           {answerCount > 0 &&
